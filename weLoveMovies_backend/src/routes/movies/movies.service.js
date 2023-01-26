b1f5ca0f1178b1def99feab02f8355074f3f9c07
amiri02 <113220653+amiri02@users.noreply.github.com>
2023-01-26 16:26:52 -0500

    url fix

--- weLoveMovies_backend/src/routes/movies/movies.service.js
+++ weLoveMovies_backend/src/routes/movies/movies.service.js
@@ -4,12 +4,7 @@ const reduceProperties = require('../../utils/reduce-properties');
 
 function list() {
     return knex('movies')
-        .select('movie_id', 
-            'title', 
-            'runtime_in_minutes', 
-            'rating', 
-            'description', 
-            'image_url');
+        .select('*');
 }
 
 function listIsShowing() {
