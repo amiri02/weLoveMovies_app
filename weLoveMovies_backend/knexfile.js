e668bbbbe3f1b94a98571c0d1e4ca3164be6101e
amiri02 <113220653+amiri02@users.noreply.github.com>
2023-01-25 22:06:57 -0500

    database

--- weLoveMovies_backend/knexfile.js
+++ weLoveMovies_backend/knexfile.js
@@ -3,8 +3,14 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgres://znhpcuzk:6aZL7IZl3m0KsbCX2JZAjfQ0jbZrJS-i@floppy.db.elephantsql.com/znhpcuzk",
-} = process.env;
+  NODE_ENV = "development",
+  DEVELOPMENT_DATABASE_URL, 
+  PRODUCTION_DATABASE_URL,
+  } = process. env;
+  const URL =
+  NODE_ENV === "production"
+  ? PRODUCTION_DATABASE_URL 
+  : DEVELOPMENT_DATABASE_URL;
 
 module.exports = {
   development: {
